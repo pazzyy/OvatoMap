ea12ec010d901127042e582c47e70ea7216ef229
Migrated - Pascal Pezet <pascal.pezet@PascalPezet-MBP.local>
2019-03-28 17:45:16 +1100

    cleaned code and created constants file

---
+++ constants.js
@@ -0,0 +1,41 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicGF6enoiLCJhIjoiY2p0aTR5b2FjMDcwMDQzbDk4b2l5OXZ0cCJ9.MG1w6Xn4FvvZC8lrB6lVNA';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/pazzz/cjtp37hn00hi61fs5jem1jd8x',
+    center: [150.938272, -33.838341],
+    zoom: 10.3
+});
+
+const layersData = {
+       DT_0204: {
+            layerIds: [],
+            menuName: 'Old DTs'
+        }, 
+        DT_0801: {
+            layerIds:[],
+            menuName: 'New DTs'
+        },
+        'DT in common': {
+            layerIds:[],
+            menuName:'Overlap'
+        },
+        DT_outlines: {
+            layerIds:[],
+            menuName:'DT Outlines'
+        },
+        road: {
+            layerIds: [],
+            menuName: 'Roads'
+        },
+        tunnel: {
+            layerIds: [],
+            menuName: 'Tunnels'
+        },
+        bridge: {
+            layerIds: [],
+            menuName: 'Bridges'
+        }
+    };
+
+
--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicGF6enoiLCJhIjoiY2p0aTR5b2FjMDcwMDQzbDk4b2l5OXZ0cCJ9.MG1w6Xn4FvvZC8lrB6lVNA';
-
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/pazzz/cjtp37hn00hi61fs5jem1jd8x',
-    center: [150.938272, -33.838341],
-    zoom: 10.3
-});
-
 map.on('load', function () {
+    map.removeLayer('DT in common copy');
     console.log(map.getStyle())
+
 
     //Displays the DT information on mouseover
     map.on('mousemove', function (e) {
@@ -27,60 +20,51 @@ map.on('load', function () {
         };
     });
 
-    const layersData = {
-       DT_0204: {
-            layerIds: ['DT_0204'],
-            menuName: 'Old DTs'
-        }, 
-        DT_0801: {
-            layerIds:['DT_0801'],
-            menuName: 'New DTs'
-        },
-        DT_Common: {
-            layerIds:['DT in common'],
-            menuName:'Overlap'
-        },
-        roads: {
-            layerIds: [],
-            menuName: 'Roads'
-        }
-    }
 
+    //Calls all layers used in the style 
     const mapLayers = map.getStyle().layers;
-    mapLayers.forEach(mapLayer => {
-        if (mapLayer["source-layer"] === "road" && mapLayer.id !== 'road-label') {
-                layersData.roads.layerIds.push(mapLayer.id)
-        }
-    })
 
+
+    //Goes through each keys of the layersData object to create a menu item out of the menuName and will also run a function when each item is clicked to check the visibility of the layer by using its ID
     Object.keys(layersData).forEach(key => {
-        var layerData = layersData[key]
 
+        mapLayers.forEach(mapLayer => {
+            if (mapLayer.id.includes(key) && mapLayer.id !== 'road-label') {
+                layersData[key].layerIds.push(mapLayer.id);
+            }
+        })
+
+        //Creates the menu items
         var link = document.createElement('a');
         link.href = '#';
         link.className = 'active';
-        link.id = layerData.menuName;
-        link.textContent = layerData.menuName;
+        link.id = layersData[key].menuName;
+        link.textContent = layersData[key].menuName;
 
+        var layers = document.getElementById('menu');
+        layers.appendChild(link);
+
+        //function that triggers when one of the item is clicked on
         link.onclick = function (e) {
             e.preventDefault();
             e.stopPropagation();
 
-            layerData.layerIds.forEach(id => {
+            //loops through each item in the layerIds array of each key to check the visibility of that item (each item is a layer represented by its Id)
+            layersData[key].layerIds.forEach(id => {
+
+                // stores the visibility property of the layer in a variable
                 var visibility = map.getLayoutProperty(id, 'visibility');
 
-            if (visibility === 'visible') {
-                map.setLayoutProperty(id, 'visibility', 'none');
-                this.className = '';
-            } else {
-                this.className = 'active';
-                map.setLayoutProperty(id, 'visibility', 'visible');
-            }
-            })
+                //compare the visibility value and change it
+                if (visibility === 'visible') {
+                    map.setLayoutProperty(id, 'visibility', 'none');
+                    this.className = '';
+                } else {
+                    this.className = 'active';
+                    map.setLayoutProperty(id, 'visibility', 'visible');
+                };
+            });
         };
-
-        var layers = document.getElementById('menu');
-        layers.appendChild(link);
     });
 
     map.getCanvas().style.cursor = 'default';
